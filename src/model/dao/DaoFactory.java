7a7e5b564f5eb8ff25c14a36c3430a2b464e7dcd
JulioCesarZbz <juliocesar_tkd@hotmail.com>
2020-12-11 00:49:33 -0300

    findById Implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,13 @@
 package model.dao;
 
+import com.mysql.jdbc.Connection;
+
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC((Connection) DB.getConnection());
 	}
 }
